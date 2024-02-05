6563937c901f39a6a6964a6b310e3cbaab617f71
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-05 21:17:42 +0530

    add buttons

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,29 @@
+import { Dispatch, SetStateAction } from 'react'
 import './Navbar.css'
 
-const Navbar = () => {
+const Navbar = ({
+  setCurrentPage,
+  currentPage,
+}: {
+  setCurrentPage: Dispatch<SetStateAction<number>>
+  currentPage: number
+}) => {
   return (
     <nav data-testid="navbar">
       <div className="nav-container">
         <div className="logo">Y</div>
         <div className="header">Hacker News</div>
-        <div className="link">new</div>|<div className="link">past</div>
+        <button onClick={() => setCurrentPage(1)} className="link button">
+          new
+        </button>
+        |
+        <button
+          disabled={currentPage === 1}
+          onClick={() => setCurrentPage((prev) => prev - 1)}
+          className="link button"
+        >
+          past
+        </button>
       </div>
       <div className="link">login</div>
     </nav>
