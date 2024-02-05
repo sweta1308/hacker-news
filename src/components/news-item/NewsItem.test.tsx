d34cc8e812bad58b7afb4ab8e84f67f4da6d1dc9
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-05 19:07:10 +0530

    add test for news item

---
+++ src/components/news-item/NewsItem.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import NewsItem from './NewsItem'
+import { NewsType } from 'pages/homepage/Home.types'
+
+const mockNews: NewsType = {
+  title: 'Mock News Title',
+  url: 'https://example.com',
+  score: 42,
+  by: 'mockuser',
+  time: Date.now() / 1000,
+  descendants: 5,
+  id: 0,
+  kids: [],
+  type: '',
+}
+
+it('renders NewsItem component with mock data', () => {
+  render(<NewsItem news={mockNews} />)
+
+  const newsItem = screen.getByTestId('news-item')
+  expect(newsItem).toBeInTheDocument()
+
+  const title = screen.getByText(mockNews.title)
+  expect(title).toBeInTheDocument()
+
+  const url = screen.getByText(`(${mockNews.url})`)
+  expect(url).toBeInTheDocument()
+
+  const by = screen.getByText(mockNews.by)
+  expect(by).toBeInTheDocument()
+
+  const descendants = screen.getByText(`${mockNews.descendants} comments`)
+  expect(descendants).toBeInTheDocument()
+})
