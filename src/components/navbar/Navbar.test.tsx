6a5d7461eb96947efac839e86b71b354c48f846a
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-05 19:06:30 +0530

    add test for navbar

---
+++ src/components/navbar/Navbar.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Navbar from './Navbar'
+
+test('renders Navbar component', () => {
+  render(<Navbar />)
+
+  const navbarElement = screen.getByTestId('navbar')
+  expect(navbarElement).toBeInTheDocument()
+
+  const logoElement = screen.getByText('Y')
+  expect(logoElement).toBeInTheDocument()
+
+  const headerElement = screen.getByText('Hacker News')
+  expect(headerElement).toBeInTheDocument()
+
+  const loginElement = screen.getByText('login')
+  expect(loginElement).toBeInTheDocument()
+
+  const newLinkElement = screen.getByText('new')
+  expect(newLinkElement).toBeInTheDocument()
+
+  const pastLinkElement = screen.getByText('past')
+  expect(pastLinkElement).toBeInTheDocument()
+})
