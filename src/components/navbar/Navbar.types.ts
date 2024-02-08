15d1ad7a722e4a0d8d402b46c71ebe37a8a9e95d
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-08 12:58:57 +0530

    remove files

--- src/components/navbar/Navbar.types.ts
+++
@@ -1,8 +0,0 @@
-import { NewsType } from 'pages/homepage/Home.types'
-import { Dispatch, SetStateAction } from 'react'
-
-export type NavbarProps = {
-  setCurrentPage: Dispatch<SetStateAction<number>>
-  currentPage: number
-  newsData: NewsType[]
-}
--- src/components/news-list/NewsList.types.ts
+++
@@ -1,9 +0,0 @@
-import { NewsType } from 'pages/homepage/Home.types'
-import { Dispatch, SetStateAction } from 'react'
-
-export type NewsListProps = {
-  newsData: NewsType[]
-  currentPage: number
-  setCurrentPage: Dispatch<SetStateAction<number>>
-  idList: number[]
-}
