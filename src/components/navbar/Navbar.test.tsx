f970374288191e9c18115c34fec476d3b57779a1
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-08 12:58:37 +0530

    add tests for navbar

--- src/components/navbar/Navbar.test.tsx
+++ src/components/navbar/Navbar.test.tsx
@@ -1,32 +1,72 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Navbar from './Navbar'
+import { useNews } from 'context/NewsContext'
 
-test('renders Navbar component', () => {
-  render(
-    <Navbar
-      setCurrentPage={(prev) => console.log(prev)}
-      currentPage={1}
-      newsData={[]}
-    />,
-  )
+jest.mock('../../context/NewsContext', () => ({
+  useNews: jest.fn(() => ({
+    currentPage: 1,
+    newsData: [
+      {
+        title: 'Mock News Title',
+        url: 'https://example.com',
+        score: 42,
+        by: 'mockuser',
+        time: Date.now() / 1000,
+        descendants: 5,
+        id: 0,
+        kids: [],
+        type: '',
+      },
+    ],
+    handleNewClick: jest.fn(),
+    handlePastClick: jest.fn(),
+  })),
+}))
 
-  const navbarElement = screen.getByTestId('navbar')
-  expect(navbarElement).toBeInTheDocument()
+describe('Navbar component', () => {
+  it('renders Navbar component', () => {
+    render(<Navbar />)
 
-  const logoElement = screen.getByText('Y')
-  expect(logoElement).toBeInTheDocument()
+    const navbarElement = screen.getByTestId('navbar')
+    expect(navbarElement).toBeInTheDocument()
 
-  const headerElement = screen.getByText('Hacker News')
-  expect(headerElement).toBeInTheDocument()
+    const logoElement = screen.getByText('Y')
+    expect(logoElement).toBeInTheDocument()
 
-  const loginElement = screen.getByText('login')
-  expect(loginElement).toBeInTheDocument()
+    const headerElement = screen.getByText('Hacker News')
+    expect(headerElement).toBeInTheDocument()
 
-  const newLinkElement = screen.getByText('new')
-  expect(newLinkElement).toBeInTheDocument()
+    const loginElement = screen.getByText('login')
+    expect(loginElement).toBeInTheDocument()
 
-  const pastLinkElement = screen.getByText('past')
-  expect(pastLinkElement).toBeInTheDocument()
+    const newLinkElement = screen.getByText('new')
+    expect(newLinkElement).toBeInTheDocument()
+
+    const pastLinkElement = screen.getByText('past')
+    expect(pastLinkElement).toBeInTheDocument()
+  })
+
+  it('calls handleNewClick when "new" button is clicked', () => {
+    const handleNewClickMock = jest.fn()
+    ;(useNews as jest.Mock).mockReturnValue({
+      handleNewClick: handleNewClickMock,
+    })
+    render(<Navbar />)
+
+    const newBtn = screen.getByText('new')
+    fireEvent.click(newBtn)
+    expect(handleNewClickMock).toHaveBeenCalled()
+  })
+
+  it('calls handlePastClick when "past" button is clicked', () => {
+    const handlePastClickMock = jest.fn()
+    ;(useNews as jest.Mock).mockReturnValue({
+      handlePastClick: handlePastClickMock,
+    })
+    render(<Navbar />)
+    const pastBtn = screen.getByText('past')
+    fireEvent.click(pastBtn)
+    expect(handlePastClickMock).toHaveBeenCalled()
+  })
 })
