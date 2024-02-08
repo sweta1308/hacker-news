e06f584b57035901c7d713b171bb57b88ace08e4
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-08 13:01:10 +0530

    wrap Home in NewsProvider

--- src/pages/homepage/Home.test.tsx
+++ src/pages/homepage/Home.test.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { screen, render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { Home } from './Home'
+import { NewsProvider } from 'context/NewsContext'
 
 test('renders Navbar and NewsList components', () => {
-  render(<Home />)
+  render(
+    <NewsProvider>
+      <Home />
+    </NewsProvider>,
+  )
 
   const navbar = screen.getByTestId('navbar')
   expect(navbar).toBeInTheDocument()
