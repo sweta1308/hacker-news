5a26e30c9453e0edc388607c87521c04ee2643b6
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-08 12:58:26 +0530

    wrap App in NewsProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { NewsProvider } from 'context/NewsContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
-root.render(<App />)
+root.render(
+  <NewsProvider>
+    <App />
+  </NewsProvider>,
+)
