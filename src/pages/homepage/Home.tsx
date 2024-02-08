69a6cf0fd5ee39fd6730ab64499f491cad24a7dd
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-08 13:01:20 +0530

    remove props

--- src/pages/homepage/Home.tsx
+++ src/pages/homepage/Home.tsx
@@ -1,23 +1,11 @@
 import { Navbar, NewsList } from '../../components/index'
 import './Home.css'
-import { useHome } from './useHome'
 
 export const Home = () => {
-  const { newsData, currentPage, setCurrentPage, idList } = useHome()
-
   return (
     <div className="home">
-      <Navbar
-        setCurrentPage={setCurrentPage}
-        currentPage={currentPage}
-        newsData={newsData}
-      />
-      <NewsList
-        newsData={newsData}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-        idList={idList}
-      />
+      <Navbar />
+      <NewsList />
     </div>
   )
 }
