b309766cb3f4272dd257454b735e1454e0777bcd
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-09 19:39:11 +0530

    remove files

--- src/utils/past-button/PastBtnDisabled.ts
+++
@@ -1,4 +0,0 @@
-import { NewsType } from 'context/NewsContext.types'
-
-export const isPastBtnDisabled = (currentPage: number, news: NewsType[]) =>
-  currentPage === 1 || news?.length === 0 ? true : false
--- src/utils/serial-number/SerialNumber.ts
+++
@@ -1,2 +0,0 @@
-export const getSerialNumber = (index: number, page: number) =>
-  (page - 1) * 30 + index + 1
